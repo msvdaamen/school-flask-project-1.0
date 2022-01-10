c26fa88766b1058b2293e1fb51a0e5ec98457672
Mischa <msvdaamen@outlookc.om>
2022-01-10 12:31:45 +0100

    Load movies with db

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,10 @@
 from flask import render_template, Blueprint
 
+from movies.models.movie import Movie
+
 bp = Blueprint('movies', __name__, url_prefix='/movies', template_folder='templates')
 
 @bp.get("/popular")
 def showPopular():
-    return render_template('popular.html')
+    movies = Movie.query.all()
+    return render_template('popular.html', movies=movies)
