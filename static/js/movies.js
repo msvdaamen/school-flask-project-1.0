4a3ba64f341daa08b9f8eb3313b3d31c6fbdc5d0
Mischa <msvdaamen@outlookc.om>
2022-01-17 15:41:05 +0100

    Fix

--- static/js/movies.js
+++ static/js/movies.js
@@ -5,7 +5,6 @@ const movieCards = document.getElementById('movies-container');
 
 let movieModalData = null;
 
-movieModal.toggle();
 movieCards.addEventListener('click', async (event) => {
     const target = isCardClick(event.path);
     if (target) {
@@ -19,6 +18,15 @@ movieCards.addEventListener('click', async (event) => {
 modalEl.addEventListener('hide.bs.modal', () => {
     console.log('hide modal');
 })
+
+const modalCoverInput = document.getElementById('movie-modal-cover-upload');
+const modalBannerInput = document.getElementById('movie-modal-banner-upload');
+const modalTitleInput = document.getElementById('movie-modal-title-input');
+const modalDateInput = document.getElementById('movie-modal-date-input');
+const modalSaveButton = document.getElementById('movie-modal-submit-button');
+
+const modalCoverPreview = document.getElementById('movie-modal-cover-preview');
+const modalBannerPreview = document.getElementById('movie-modal-banner-preview');
 
 // document.getElementById('movie-modal-save-button').addEventListener('click', async () => {
 //     const id = movieModalData.id;
@@ -34,32 +42,43 @@ modalEl.addEventListener('hide.bs.modal', () => {
 //     }).then(r => r.json());
 //     console.log(response)
 // });
-
-
-document.getElementById('movie-modal-preview-banner').addEventListener('click', () => {
-    document.getElementById('movie-modal-banner-file-upload').click();
+modalCoverPreview.addEventListener('click', () => {
+    modalCoverInput.click();
 });
-document.getElementById('movie-modal-cover-file-upload').addEventListener('change', (event) => {
+modalBannerPreview.addEventListener('click', () => {
+    modalBannerInput.click();
+});
+modalCoverInput.addEventListener('change', (event) => {
+    setModalPreviewCover();
+});
+modalBannerInput.addEventListener('change', (event) => {
     setModalPreviewBanner();
 });
-document.getElementById('movie-modal-banner-file-upload').addEventListener('change', (event) => {
-    setModalPreviewBanner();
+modalTitleInput.addEventListener('keyup', () => {
+    checkCanSaveMovie();
 });
-document.getElementById('movie-modal-preview-banner-preview').addEventListener('click', () => {
-    document.getElementById('movie-modal-banner-file-upload').click();
+modalDateInput.addEventListener('keyup', () => {
+    checkCanSaveMovie();
 });
 
-function setModalPreviewBanner() {
-    if (document.getElementById('movie-modal-preview-banner')) {
-        document.getElementById('movie-modal-preview-banner').remove();
+movieModal.toggle();
+checkCanSaveMovie();
+
+function checkCanSaveMovie() {
+    if (!modalTitleInput.value.trim() || !modalDateInput.value.trim()) {
+        modalSaveButton.disabled = true;
+        return;
     }
-    const file = document.getElementById('movie-modal-banner-file-upload').files[0];
-    document.getElementById('movie-modal-preview-banner-preview').src = URL.createObjectURL(file);
+    modalSaveButton.disabled = false;
 }
 
 function setModalPreviewCover() {
-    const file = document.getElementById('movie-modal-cover-upload').files[0];
-    document.getElementById('movie-modal-preview-caver-preview').src = URL.createObjectURL(file);
+    const file = modalCoverInput.files[0];
+    modalCoverPreview.src = URL.createObjectURL(file);
+}
+function setModalPreviewBanner() {
+    const file = modalBannerInput.files[0];
+    modalBannerPreview.src = URL.createObjectURL(file);
 }
 
 
